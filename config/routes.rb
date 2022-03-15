e0147bf23cf6b3b70a9a47d3be4a42170c679ae1
juliends <julien@lewagon.com>
2022-03-15 12:11:10 +0100

    Front end setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants do
+    collection do
+      get :top
+    end
+  end
 end
